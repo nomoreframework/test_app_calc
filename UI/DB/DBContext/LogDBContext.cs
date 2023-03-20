7269a8489e300caa847dbc96924b10a238a9b75a
nomoreframework <gleblukianenko@gmail.com>
2023-03-20 22:19:27 +0300

    added context and rename binding property

---
+++ UI/DB/DBContext/LogDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Calculator.Models.LogModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calculator.DB.DBContext
+{
+    class LogDBContext : DbContext
+    {
+
+        public DbSet<LogModel> OpErrors { get; set; } = null!;
+        public DbSet<LogModel> SysErrors { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=calculator_logs.db");
+        }
+    }
+}
